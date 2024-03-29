4ce81d0cd45443dd7538d487034943524af02ce2
Fauzan Nursalma <55134999+fauzannursalma@users.noreply.github.com>
2024-03-29 15:24:28 +0700

    fix: Owl Carousel [It's Worked on Local but doesn't work on github pages

--- src/script/script.js
+++ src/script/script.js
@@ -10,3 +10,35 @@ for (let i = 0; i < navToggler.length; i++) {
     document.getElementById("overlay").classList.toggle("hidden");
   });
 }
+
+// Owl Carousel (Testimonials)
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 5,
+  center: true,
+  items: 2,
+  autoWidth: true,
+  responsiveClass: true,
+  responsive: {
+    0: {
+      items: 1,
+      nav: false,
+    },
+    1000: {
+      items: 3,
+      nav: false,
+    },
+  },
+});
+
+// Owl Carousel (Chevron)
+var owl = $(".owl-carousel");
+owl.owlCarousel();
+// next chevron
+$("#nextBtn").on("click", function () {
+  owl.trigger("next.owl.carousel");
+});
+// previous chevron
+$("#prevBtn").on("click", function () {
+  owl.trigger("prev.owl.carousel", [300]);
+});
